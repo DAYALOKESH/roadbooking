25c31c6b471156cc1b362b566c7e501ec4e578cd
Sibin <sibin.georgge@gmail.com>
2025-04-16 12:16:36 +0100

    FInal

--- regional_manager/ireland/database.py
+++ regional_manager/ireland/database.py
@@ -8,6 +8,8 @@ Base = declarative_base()
 
 # Database setup
 DATABASE_URL = "postgresql://postgres:12345678@35.189.66.196:5432/osm"
+# DATABASE_URL = "postgresql://postgres:psotgres@192.168.118.5:5433/road_capacity"
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- regional_manager/london/database.py
+++ regional_manager/london/database.py
@@ -9,6 +9,8 @@ Base = declarative_base()
 # Database setup
 
 DATABASE_URL = "postgresql://postgres:sustainablecitymanagement0@34.46.53.83:5432/osm"
+# DATABASE_URL = "postgresql://postgres:psotgres@192.168.118.5:5434/london_road_capacity"
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
